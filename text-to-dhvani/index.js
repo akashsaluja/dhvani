1b19000677bf52a85f7a98fe37ec5022163b0558
Akash Saluja <akashsaluja@outlook.com>
2017-12-16 17:30:35 +0400

    Some more debugging

--- text-to-dhvani/index.js
+++ text-to-dhvani/index.js
@@ -1,29 +1,24 @@
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-    context.res = {
-        status: 200,
-        body: 'Hey AKash'
-    };
-    // if (req.query.name || (req.body && req.body.name)) {
-    //     context.log('Came here 1');
-    //     request('http://www.bing.com', function (error, response, body) {
-    //     // This callback function will never be called
-    //         if (err) {
-    //             context.log(err);
-    //         }
-
-    //         if (!error && response.statusCode == 200) {
-    //             context.log(body) // Show the HTML for the Bing homepage. 
-    //         }
-    //         context.done();
-    //     });
-    // }
-    // else {
-    //     context.log('Came here 2');
-    //     context.res = {
-    //         status: 400,
-    //         body: "Please pass a name on the query string or in the request body"
-    //     };
-    // }
-    context.done();
+    if (req.query.name || (req.body && req.body.name)) {
+        context.log('Came here 1');
+        const myRequest = new Request('http://www.bing.com', {method: 'GET'});
+        fetch(myRequest).
+        then(response => {
+            context.res = {
+                status: 200,
+                body: response
+            };
+            context.done();
+        });
+    }
+    else {
+        context.log('Came here 2');
+        context.res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+        context.done();
+    }
+    
 };
