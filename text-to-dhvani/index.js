8e4991e66ca2c78c2ade27dcb0c35a861a1df6ed
Akash Saluja <akashsaluja@outlook.com>
2017-12-16 14:21:46 +0400

    More changes

--- text-to-dhvani/index.js
+++ text-to-dhvani/index.js
@@ -1,26 +1,25 @@
+var request = require('request');
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     if (req.query.name || (req.body && req.body.name)) {
-        var client = new HttpClient();
-        var url = "http:\/\/google.com";
-        client.get(url, function(response) {
-            // do something with response
-            context.res = {
-                // status: 200, /* Defaults to 200 */
-                body: response
-                context.done();
-            };
-        });
+        request('http://www.bing.com', function (error, response, body) {
+        // This callback function will never be called
+            if (err) {
+                context.log(err);
+            }
 
-        
+            if (!error && response.statusCode == 200) {
+                context.log(body) // Show the HTML for the Bing homepage. 
+            }
+            context.done();
+        });
     }
     else {
         context.res = {
             status: 400,
             body: "Please pass a name on the query string or in the request body"
         };
-        context.done();
     }
-   
+    context.done();
 };
