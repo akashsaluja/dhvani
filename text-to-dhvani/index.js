8233d572fdc294077cf6e1a0b14454f5cf0f82a5
Akash Saluja <akashsaluja@outlook.com>
2017-12-16 14:13:34 +0400

    Changes

--- text-to-dhvani/index.js
+++ text-to-dhvani/index.js
@@ -2,22 +2,25 @@ module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            var client = new HttpClient();
-            var url = "http:\/\/google.com";
-            client.get(url, function(response) {
-                // do something with response
+        var client = new HttpClient();
+        var url = "http:\/\/google.com";
+        client.get(url, function(response) {
+            // do something with response
+            context.res = {
+                // status: 200, /* Defaults to 200 */
                 body: response
-            });
+                context.done();
+            };
+        });
 
-        };
+        
     }
     else {
         context.res = {
             status: 400,
             body: "Please pass a name on the query string or in the request body"
         };
+        context.done();
     }
-    context.done();
+   
 };
