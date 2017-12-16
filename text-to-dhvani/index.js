cc5f37479ae162eea6bc0d819888a49d96558de6
Akash Saluja <akashsaluja@outlook.com>
2017-12-17 00:15:28 +0400

    Changed response

--- text-to-dhvani/index.js
+++ text-to-dhvani/index.js
@@ -9,7 +9,7 @@ module.exports = function (context, req) {
             context.log('Came here 3');
             context.res = {
                 status: 200,
-                body: JSON.stringify(response)
+                body: "JSON.stringify(response)"
             };
             context.log('Came here 4');
             context.done();
