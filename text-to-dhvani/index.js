d8f66b761081447f8c96b6d335298a3ff9f52502
Akash Saluja <akashsaluja@outlook.com>
2017-12-16 19:14:11 +0400

    Updated

--- text-to-dhvani/index.js
+++ text-to-dhvani/index.js
@@ -1,15 +1,14 @@
-var request = require('request');
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     if (req.query.name || (req.body && req.body.name)) {
         context.log('Came here 1');
-        request('http://www.bing.com', function (error, response, body) {
+        // request('http://www.bing.com', function (error, response, body) {
             context.res = {
                 status: 200,
-                body: response
+                body: "response"
             };
             context.done();
-          });
+        //   });
     }
     else {
         context.log('Came here 2');
