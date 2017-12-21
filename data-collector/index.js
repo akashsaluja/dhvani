58929fb82017503c4747739470f0bad1e21d053f
Akash Saluja <akashsaluja@outlook.com>
2017-12-21 22:29:21 +0400

    Updated code

--- data-collector/index.js
+++ data-collector/index.js
@@ -2,17 +2,17 @@ var request = require('request');
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     if (req.query.name || (req.body && req.body.name)) {
-        context.log('Came here 1');
-        request('http://www.bing.com', function (error, response, body) {
+        var request = require('request');
+        context.log('Came here 2');
+        request('http://www.google.com', function (error, response, body) {
+            context.log('Came here 3');
             response.setEncoding('utf8');
-            response.on('data', function (chunk) {
-                context.res = {
+            context.log('Came here 5');
+            context.res = {
                     status: 200,
-                    body: JSON.stringify(chunk)
-                };
-                context.log('Came here 4');
-                context.done();
-            });
+                    body: body
+            };
+            context.done();
         });
     }
     else {
