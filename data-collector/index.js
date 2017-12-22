1fe65e2bfd3d5bd89e73efcd5750c9a6e26d1904
Akash Saluja <akashsaluja@outlook.com>
2017-12-22 10:48:41 +0400

    Added functionality for parsing news

--- data-collector/index.js
+++ data-collector/index.js
@@ -1,29 +1,13 @@
 var request = require('request');
 module.exports = function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-    if (req.query.name || (req.body && req.body.name)) {
-        var request = require('request');
-        context.log('Came here 2');
-        request('http://www.google.com', function (error, response, body) {
-            context.log('Came here 3');
-            response.setEncoding('utf8');
-            context.log('Came here 5');
-            context.res = {
-                    status: 200,
-                    body: body
-            };
-            context.bindings.outputQueueItem = "Name passed to the function: " + 
-            (req.query.name || req.body.name);
+    context.log('Data Collector functions has started functioning.');
+    var inshorts = require('./news/inshorts');
+    inshorts.getDataInshorts().then(
+        (items) => {
+            console.log(items);
             context.done();
-        });
-    }
-    else {
-        context.log('Came here 2');
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-        context.done();
-    }
-    
+        }
+    );
+    // context.bindings.outputQueueItem = "Name passed to the function: " + 
+    // (req.query.name || req.body.name);
 };
