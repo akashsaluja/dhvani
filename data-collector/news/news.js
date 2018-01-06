a2b36bc0529b00888f87ff0f537f2e7363b2a962
Akash Saluja <akashsaluja@outlook.com>
2018-01-06 14:43:33 +0400

    Deleted content

--- data-collector/news/news.js
+++
@@ -1,7 +0,0 @@
-module.exports.News =  class News {
-    constructor(headline, text, timestamp) {
-      this.headline = headline;
-      this.text = text;
-      this.timestamp = timestamp;
-    }
-  }
