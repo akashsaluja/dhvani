44ba7e7ef6688de8ad09ff69f50f0cb8be26663b
Akash Saluja <akashsaluja@outlook.com>
2018-01-07 16:38:10 +0400

    Added loginc to create filkes

--- dhvanification.py
+++ dhvanification.py
@@ -1,18 +1,28 @@
 from content import Content
 from bingtts import Translator
+from pydub import AudioSegment
 import os
 
 def dhvanify(content):
     translator = Translator(os.environ['BING_SPEECH_ACCESS_KEY'])
-    print(os.environ['BING_SPEECH_ACCESS_KEY'])
     audioOutputs = []
     finalText = []
     for idx, val in enumerate(content.newsItems):
         val.body = ("Story #%s: " %(idx)) + val.body + "       "
         finalText.append(val.body)
-    for newsItem in finalText:
-        print(newsItem)
-    output  = translator.speak(finalText[0], "en-US", "Female", "riff-16khz-16bit-mono-pcm")
-    with open("file.wav", "wb") as f:
-        f.write(output)
+    for idx, text in enumerate(finalText):
+        output = translator.speak(text, "en-US", "Female", "riff-16khz-16bit-mono-pcm")
+        with open("tmp/file%s.wav"%(idx), "wb") as f:
+            f.write(output)
+        audioOutputs.append(AudioSegment.from_wav("tmp/file%s.wav"%(idx)))
+    
+    for idx, snippet in enumerate(audioOutputs):
+        if(idx == 0):
+            finalAudio = snippet
+        else:
+            finalAudio = finalAudio + snippet
+    finalAudio.export("tmp/final.wav", format="wav")
+
+    
+    
     
